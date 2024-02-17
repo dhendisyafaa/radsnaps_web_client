29a509a158e6c73cb526d2340b7de9dc681e3acb
Dhendi Syafa Athallah Putra <dhendisyafa@gmail.com>
2024-02-17 17:39:01 +0700

    feat: add constants for some data components

---
+++ src/constants/data.ts
@@ -0,0 +1,73 @@
+export const FILTERIMAGE = [
+  {
+    label: "Trending",
+    icon: "flame",
+    href: "/gallery?filter=trending",
+  },
+  {
+    label: "Newest",
+    icon: "sparkles",
+    href: "/gallery?filter=newest",
+  },
+];
+
+export const NAVSIDEBARITEMS = [
+  {
+    title: "Dashboard",
+    href: "/dashboard",
+    icon: "dashboard",
+    label: "Dashboard",
+  },
+  {
+    title: "Official Content",
+    href: "/dashboard/official-content",
+    icon: "user",
+    label: "user",
+  },
+  {
+    title: "Report Content",
+    href: "/dashboard/report-content",
+    icon: "flag",
+    label: "Report",
+  },
+  {
+    title: "Settings",
+    href: "/dashboard/settings",
+    icon: "settings",
+    label: "setting",
+  },
+];
+
+export const REPORTISSUES = [
+  {
+    label: "Hate, discrimination, hate symbols and logos",
+    value: "Hate, discrimination, hate symbols and logos",
+  },
+  { label: "Insults & harassment", value: "Insults & harassment" },
+  { label: "Privacy", value: "Privacy" },
+  {
+    label: "Sensitive & disturbing media",
+    value: "Sensitive & disturbing media",
+  },
+  { label: "Suicide or self-harm", value: "Suicide or self-harm" },
+  {
+    label: "Violent & hateful entities",
+    value: "Violent & hateful entities",
+  },
+  {
+    label: "Racist or sexist stereotypes",
+    value: "Racist or sexist stereotypes",
+  },
+  {
+    label: "Unwanted sexual content & graphic objectification",
+    value: "Unwanted sexual content & graphic objectification",
+  },
+  {
+    label: "Violent speech, wishing for harm, glorifying violence",
+    value: "Violent speech, wishing for harm, glorifying violence",
+  },
+  {
+    label: "Adult nudity & sexual behavior",
+    value: "Adult nudity & sexual behavior",
+  },
+];
