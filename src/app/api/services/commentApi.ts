e70404121f86a56e437870ec5b734a0924b1f34a
Dhendi Syafa Athallah Putra <dhendisyafa@gmail.com>
2024-02-20 12:09:50 +0700

    feat: change endpoint comment by image id

--- src/app/api/services/commentApi.ts
+++ src/app/api/services/commentApi.ts
@@ -2,7 +2,7 @@ import axios from "@/lib/axios";
 import { convertToURI } from "@/utils/convertObjectToURI";
 
 export const getCommentByImage = (image_id, params = {}) => {
-  return axios.get(`/comment/${image_id}`);
+  return axios.get(`/comment/image/${image_id}`);
 };
 
 export const createCommentByImage = (auth, data) => {
