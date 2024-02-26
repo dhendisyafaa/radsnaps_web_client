6b2544621247c5aadc3bf0d7a41e933d14d924c3
Dhendi Syafa Athallah Putra <dhendisyafa@gmail.com>
2024-02-26 22:44:21 +0700

    featL conditional render for skeleton loading

--- src/components/common/skeleton/SkeletonGallery.tsx
+++ src/components/common/skeleton/SkeletonGallery.tsx
@@ -1,13 +1,15 @@
 import { Skeleton } from "@/components/ui/skeleton";
 import React from "react";
 
-export default function SkeletonGallery() {
+export default function SkeletonGallery({ withHeader = true }) {
   return (
     <div className="h-screen w-full flex flex-col gap-5">
-      <div className="flex justify-between gap-3">
-        <Skeleton className="h-10 w-44" />
-        <Skeleton className="h-10 w-full md:w-[30%]" />
-      </div>
+      {withHeader && (
+        <div className="flex justify-between gap-3">
+          <Skeleton className="h-10 w-44" />
+          <Skeleton className="h-10 w-full md:w-[30%]" />
+        </div>
+      )}
       <div className="columns-2 gap-3 lg:gap-5 space-y-5 sm:columns-3 lg:columns-4 xl:columns-5">
         <Skeleton className="w-full h-72" />
         <Skeleton className="w-full h-72" />
