e7c20ecb9148f9505f373eea9dc670bedb69d22a
Dhendi Syafa Athallah Putra <dhendisyafa@gmail.com>
2024-03-02 22:30:21 +0700

    feat: add icon for button upload image

--- src/components/button/ButtonPostImage.tsx
+++ src/components/button/ButtonPostImage.tsx
@@ -1,9 +1,11 @@
+import { LucideUpload } from "lucide-react";
 import Link from "next/link";
 
 export default function ButtonPostImage() {
   return (
     <Link href={"/posting"}>
-      <div className="bg-primary rounded-full px-6 py-2 text-xs font-bold hidden md:block w-fit text-center text-white">
+      <div className="gap-1 items-center bg-primary rounded-full px-5 py-2 text-xs font-bold hidden md:flex w-fit text-center text-white [&_svg]:w-5 [&_svg]:h-5">
+        <LucideUpload />
         Posting
       </div>
     </Link>
