5ac48c49d379156e10cb116e2b0a5139ae4c0f91
Dhendi Syafa Athallah Putra <dhendisyafa@gmail.com>
2024-03-02 17:21:51 +0700

    feat: add package cloudinary and config

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -3,3 +3,4 @@ export const baseUrlWeb = process.env.NEXT_PUBLIC_BASE_URL_WEB;
 export const nextAuthSecret = process.env.NEXTAUTH_SECRET;
 export const googleOauthClientId = process.env.GOOGLE_OAUTH_CLIENT_ID;
 export const googleOauthClientSecret = process.env.GOOGLE_OAUTH_CLIENT_SECRET;
+export const cloudinaryCloudname = process.env.NEXT_PUBLIC_CLOUDINARY_CLOUDNAME;
