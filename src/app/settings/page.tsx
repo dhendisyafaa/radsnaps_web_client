4533d316ace1abfc73f17b3d9ed5080bc4f69142
Dhendi Syafa Athallah Putra <dhendisyafa@gmail.com>
2024-03-02 22:28:10 +0700

    feat: loading animation three dots

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import LoadingThreeDoots from "@/components/common/loader/LoadingThreeDoots";
 import FormEditProfileUser from "@/components/form/FormEditProfile";
 import { useUserData } from "@/hooks/useUserData";
 import { useUserByUsername } from "../api/resolver/userResolver";
@@ -7,7 +8,12 @@ import { useUserByUsername } from "../api/resolver/userResolver";
 export default function ProfileForm() {
   const { username } = useUserData();
   const { data: profileUser, isLoading } = useUserByUsername(username);
-  if (isLoading) return <p>load...</p>;
+  if (isLoading)
+    return (
+      <div className="flex justify-center">
+        <LoadingThreeDoots />
+      </div>
+    );
 
   const profile = profileUser.data.data;
 
