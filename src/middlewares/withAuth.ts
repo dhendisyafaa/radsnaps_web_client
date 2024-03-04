df0d1590493738ea420efd9dc5eca2aa1dd10954
Dhendi Syafa Athallah Putra <dhendisyafa@gmail.com>
2024-03-04 16:58:01 +0700

    fix: page only admin not working

--- src/middlewares/withAuth.ts
+++ src/middlewares/withAuth.ts
@@ -1,7 +1,9 @@
 import { nextAuthSecret } from "@/configs/config";
+import { useUserData } from "@/hooks/useUserData";
 import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
-const onlyAdminUrl = ["/dashboard/:path*"];
+
+const onlyAdminUrl = ["/dashboard", "/dashboard/:path*"];
 
 export default function withAuth(middleware, requireAuth) {
   return async (req, next) => {
